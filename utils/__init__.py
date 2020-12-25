4ef5c99622b0136bf5147467877840c00f850e38
majsylw <majsylw@onet.eu>
2020-12-25 13:53:47 +0100

    add requirements for data split

---
+++ utils/__init__.py
@@ -0,0 +1,9 @@
+from utils.dataset_converter import convert_dataset, \
+                                    concatenate_datasets, \
+                                    taco_categories_to_detectwaste
+from utils.split_coco_dataset import split_coco_dataset
+
+__all__ = [
+    'convert_dataset', 'taco_categories_to_detectwaste',
+    'split_coco_dataset', 'concatenate_datasets'
+]
