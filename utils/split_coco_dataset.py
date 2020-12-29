ce7161267cd55097de0928c6de2b9077cf0d3d19
majsylw <majsylw@onet.eu>
2020-12-29 18:46:51 +0100

    add multilabel cross validators based on an implementation of the Iterative Stratification algorithm

--- utils/split_coco_dataset.py
+++ utils/split_coco_dataset.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import StratifiedShuffleSplit
 import numpy as np
 from collections import defaultdict, Counter
 from utils.dataset_converter import concatenate_datasets
+from iterstrat.ml_stratifiers import MultilabelStratifiedShuffleSplit
 
 
 # filter_annotations and save_coco on akarazniewicz/cocosplit
@@ -51,10 +52,44 @@ def PseudoStratifiedShuffleSplit(images,
     return x, y
 
 
+# function based on https://github.com/trent-b/iterative-stratification'''
+def MultiStratifiedShuffleSplit(images,
+                                annotations,
+                                test_size):
+    # count categories per image
+    categories_per_image = defaultdict(Counter)
+    max_id = 0
+    for ann in annotations:
+        categories_per_image[ann['image_id']][ann['category_id']] += 1
+        if ann['category_id'] > max_id:
+            max_id = ann['category_id']
+
+    # prepare list with count of cateory objects per image
+    all_categories = []
+    for cat in categories_per_image.values():
+        pair = []
+        for i in range(1, max_id + 1):
+            pair.append(cat[i])
+        all_categories.append(pair)
+
+    # multilabel-stratified-split
+    strat_split = MultilabelStratifiedShuffleSplit(n_splits=1,
+                                                   test_size=test_size,
+                                                   random_state=2020)
+
+    for train_index, test_index in strat_split.split(images,
+                                                     all_categories):
+        x = [images[i] for i in train_index]
+        y = [images[i] for i in test_index]
+    print('Train:', len(x), 'images, valid:', len(y))
+    return x, y
+
+
 # split_coco_dataset partially based on akarazniewicz/cocosplit
 def split_coco_dataset(list_of_datasets_to_split,
                        dest,
-                       test_size=0.2):
+                       test_size=0.2,
+                       mode='multi'):
     if len(list_of_datasets_to_split) > 1:
         dataset = concatenate_datasets(list_of_datasets_to_split)
     else:
@@ -71,13 +106,15 @@ def split_coco_dataset(list_of_datasets_to_split,
                                          int(ann['image_id']), annotations)
     images = funcy.lremove(lambda i: i['id'] not in
                            images_with_annotations, images)
-
-    x, y = PseudoStratifiedShuffleSplit(images, annotations, test_size)
+    if mode == 'multi':
+        x, y = MultiStratifiedShuffleSplit(images, annotations, test_size)
+    else:
+        x, y = PseudoStratifiedShuffleSplit(images, annotations, test_size)
 
     save_coco(dest+'_train.json', info, licenses,
               x, filter_annotations(annotations, x), categories)
     save_coco(dest+'_test.json', info, licenses, y,
               filter_annotations(annotations, y), categories)
 
-    print('Finished pseudo-stratified shuffle split. Results saved in:',
+    print('Finished stratified shuffle split. Results saved in:',
           dest + '_train.json', dest + '_test.json')
