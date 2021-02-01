b550bfa4b147a1e4590b9cc4da7e0373f1ae5996
Arkadiusz Kwasigroch <51204632+akwasigroch@users.noreply.github.com>
2021-02-01 19:01:23 +0100

    add comments to configs

--- efficientdet_lighning/dataloader.py
+++ efficientdet_lighning/dataloader.py
@@ -7,7 +7,7 @@ from efficientdet_lighning.efficientdet.evaluator import create_evaluator
 def create_datasets_and_loaders(args, model_config):
     input_config = resolve_input_config(args, model_config=model_config)
 
-    dataset_train, dataset_eval = create_dataset('DetectwasteCfg', "/dih4/dih4_2/wimlds/data/")
+    dataset_train, dataset_eval = create_dataset('DetectwasteCfg', "/dih4/dih4_2/wimlds/data/") #yamlconfig
 
     # setup labeler in loader/collate_fn if not enabled in the model bench
     labeler = None
@@ -21,7 +21,7 @@ def create_datasets_and_loaders(args, model_config):
     loader_train = create_loader(
         dataset=dataset_train,
         input_size=input_config['input_size'],
-        batch_size=3,
+        batch_size=3, #yamlconfig
         is_training=True,
         use_prefetcher=True,
         re_prob=0.2,
@@ -33,7 +33,7 @@ def create_datasets_and_loaders(args, model_config):
         fill_color=input_config['fill_color'],
         mean=input_config['mean'],
         std=input_config['std'],
-        num_workers=8,
+        num_workers=8, #yamlconfig
         distributed=False,
         pin_mem=True,
         anchor_labeler=labeler,
@@ -43,14 +43,14 @@ def create_datasets_and_loaders(args, model_config):
     loader_eval = create_loader(
         dataset_eval,
         input_size=input_config['input_size'],
-        batch_size=8,
+        batch_size=8, #yamlconfig
         is_training=False,
         use_prefetcher=True,
         interpolation=input_config['interpolation'],
         fill_color=input_config['fill_color'],
         mean=input_config['mean'],
         std=input_config['std'],
-        num_workers=2,
+        num_workers=2, #yamlconfig
         distributed=True,
         pin_mem=True,
         anchor_labeler=labeler,
--- efficientdet_lighning/module.py
+++ efficientdet_lighning/module.py
@@ -14,11 +14,11 @@ class EfficientDetModule(pl.LightningModule):
     def __init__(self):
         super().__init__()
         self.model = create_model(
-            model_name='tf_efficientdet_d2',
+            model_name='tf_efficientdet_d2', #yamlconfig
             bench_task='train',
             num_classes=7,
-            pretrained=True,
-            pretrained_backbone=True,
+            pretrained=True, #yamlconfig
+            pretrained_backbone=True, #yamlconfig
             redundant_bias=None,
             label_smoothing=None,
             new_focal=None,
@@ -68,11 +68,11 @@ class EfficientDetModule(pl.LightningModule):
 
     def configure_optimizers(self, ):
         optim_parameters = {
-            'lr': 0.001,
-            'opt': 'momentum',
-            'opt-eps': 1e-3,
-            'momentum': 0.9,
-            'weight_decay': 4e-5
+            'lr': 0.001, #yamlconfig
+            'opt': 'momentum', #yamlconfig
+            'opt-eps': 1e-3, #yamlconfig
+            'momentum': 0.9, #yamlconfig
+            'weight_decay': 4e-5 #yamlconfig
         }
         optim_parameters = SimpleNamespace(**optim_parameters)
         optimizer = create_optimizer(optim_parameters, self.model)
@@ -81,22 +81,22 @@ class EfficientDetModule(pl.LightningModule):
 
 if __name__ == '__main__':
     neptune_logger = NeptuneLogger(
-        project_name='detectwaste/efficientdet-lighning',
-        experiment_name='effdet-lighning',
+        project_name='detectwaste/efficientdet-lighning', #yamlconfig
+        experiment_name='effdet-lighning', #yamlconfig
     )
     gpu_monitor = GPUStatsMonitor()
     lr_logger = LearningRateMonitor()
     module = EfficientDetModule()
-    trainer = pl.Trainer(gpus=[0, 1, 2, 3, 4, 5, 6, 7],
+    trainer = pl.Trainer(gpus=[0, 1, 2, 3, 4, 5, 6, 7], #yamlconfig
                          accelerator='ddp',
                          replace_sampler_ddp=False,
-                         gradient_clip_val=10,
+                         gradient_clip_val=10, #yamlconfig
                          logger=neptune_logger,
                          # limit_train_batches=4 * 4 * 12,
                          # limit_val_batches=0,
                          log_every_n_steps=10,
                          sync_batchnorm=True,
-                         max_epochs=50,
+                         max_epochs=50, #yamlconfig
                          callbacks=[gpu_monitor, lr_logger]
                          )
 
