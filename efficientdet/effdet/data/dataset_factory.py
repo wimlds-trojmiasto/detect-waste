1cb99eebfd568b1d582f69c077a847352644e1dd
majsylw <majsylw@onet.eu>
2021-02-13 00:54:43 +0100

    add possibility to put path from comand line

--- efficientdet/effdet/data/dataset_factory.py
+++ efficientdet/effdet/data/dataset_factory.py
@@ -11,8 +11,12 @@ from .parsers import *
 from .dataset import DetectionDatset
 from .parsers import create_parser
 
+waste_datasets_list = ['taco', 'detectwaste', 'binary', 'multi',
+                       'uav', 'mju', 'trashcan', 'wade', 'icra'
+                       'drinkwaste']
 
-def create_dataset(name, root, splits=('train', 'val')):
+
+def create_dataset(name, root, ann, splits=('train', 'val')):
     if isinstance(splits, str):
         splits = (splits,)
     name = name.lower()
@@ -38,153 +42,30 @@ def create_dataset(name, root, splits=('train', 'val')):
                 parser=create_parser(dataset_cfg.parser, cfg=parser_cfg),
             )
         datasets = OrderedDict()
-    elif name.startswith('taco'):
-        dataset_cfg = TACOCfg()
-       
-        for s in splits:
-            if s not in dataset_cfg.splits:
-                raise RuntimeError(f'{s} split not found in config')
-            split_cfg = dataset_cfg.splits[s]
-            ann_file = root / split_cfg['ann_filename']
-            parser_cfg = CocoParserCfg(
-                ann_filename=ann_file,
-                has_labels=split_cfg['has_labels']
-            )
-            datasets[s] = dataset_cls(
-                data_dir=root / Path(split_cfg['img_dir']),
-                parser=create_parser(dataset_cfg.parser, cfg=parser_cfg),
-            )
-    elif name.startswith('detectwaste'):
-        dataset_cfg = DetectwasteCfg()
-       
-        for s in splits:
-            if s not in dataset_cfg.splits:
-                raise RuntimeError(f'{s} split not found in config')
-            split_cfg = dataset_cfg.splits[s]
-            ann_file = root / split_cfg['ann_filename']
-            parser_cfg = CocoParserCfg(
-                ann_filename=ann_file,
-                has_labels=split_cfg['has_labels']
-            )
-            datasets[s] = dataset_cls(
-                data_dir=root / Path(split_cfg['img_dir']),
-                parser=create_parser(dataset_cfg.parser, cfg=parser_cfg),
-            )
-    elif name.startswith('binary'):
-        dataset_cfg = BinaryCfg()
-       
-        for s in splits:
-            if s not in dataset_cfg.splits:
-                raise RuntimeError(f'{s} split not found in config')
-            split_cfg = dataset_cfg.splits[s]
-            ann_file = root / split_cfg['ann_filename']
-            parser_cfg = CocoParserCfg(
-                ann_filename=ann_file,
-                has_labels=split_cfg['has_labels']
-            )
-            datasets[s] = dataset_cls(
-                data_dir=root / Path(split_cfg['img_dir']),
-                parser=create_parser(dataset_cfg.parser, cfg=parser_cfg),
-            )
-    elif name.startswith('multi'):
-        dataset_cfg = BinaryMultiCfg()
-       
-        for s in splits:
-            if s not in dataset_cfg.splits:
-                raise RuntimeError(f'{s} split not found in config')
-            split_cfg = dataset_cfg.splits[s]
-            ann_file = root / split_cfg['ann_filename']
-            parser_cfg = CocoParserCfg(
-                ann_filename=ann_file,
-                has_labels=split_cfg['has_labels']
-            )
-            datasets[s] = dataset_cls(
-                data_dir=root / Path(split_cfg['img_dir']),
-                parser=create_parser(dataset_cfg.parser, cfg=parser_cfg),
-            )
-    elif name.startswith('uv'):
-        dataset_cfg = UVVasteCfg()
-       
-        for s in splits:
-            if s not in dataset_cfg.splits:
-                raise RuntimeError(f'{s} split not found in config')
-            split_cfg = dataset_cfg.splits[s]
-            ann_file = root / split_cfg['ann_filename']
-            parser_cfg = CocoParserCfg(
-                ann_filename=ann_file,
-                has_labels=split_cfg['has_labels']
-            )
-            datasets[s] = dataset_cls(
-                data_dir=root / Path(split_cfg['img_dir']),
-                parser=create_parser(dataset_cfg.parser, cfg=parser_cfg),
-            )
-    elif name.startswith('trashcan'):
-        dataset_cfg = TrashCanCfg()
-       
-        for s in splits:
-            if s not in dataset_cfg.splits:
-                raise RuntimeError(f'{s} split not found in config')
-            split_cfg = dataset_cfg.splits[s]
-            ann_file = root / split_cfg['ann_filename']
-            parser_cfg = CocoParserCfg(
-                ann_filename=ann_file,
-                has_labels=split_cfg['has_labels']
-            )
-            datasets[s] = dataset_cls(
-                data_dir=root / Path(split_cfg['img_dir']),
-                parser=create_parser(dataset_cfg.parser, cfg=parser_cfg),
-            )
-    elif name.startswith('drinkwaste'):
-        dataset_cfg = DrinkWasteCfg()
-       
-        for s in splits:
-            if s not in dataset_cfg.splits:
-                raise RuntimeError(f'{s} split not found in config')
-            split_cfg = dataset_cfg.splits[s]
-            ann_file = root / split_cfg['ann_filename']
-            parser_cfg = CocoParserCfg(
-                ann_filename=ann_file,
-                has_labels=split_cfg['has_labels']
-            )
-            datasets[s] = dataset_cls(
-                data_dir=root / Path(split_cfg['img_dir']),
-                parser=create_parser(dataset_cfg.parser, cfg=parser_cfg),
-            )
-    elif name.startswith('mju'):
-        dataset_cfg = MJU_WasteCfg()
-       
-        for s in splits:
-            if s not in dataset_cfg.splits:
-                raise RuntimeError(f'{s} split not found in config')
-            split_cfg = dataset_cfg.splits[s]
-            ann_file = root / split_cfg['ann_filename']
-            parser_cfg = CocoParserCfg(
-                ann_filename=ann_file,
-                has_labels=split_cfg['has_labels']
-            )
-            datasets[s] = dataset_cls(
-                data_dir=root / Path(split_cfg['img_dir']),
-                parser=create_parser(dataset_cfg.parser, cfg=parser_cfg),
-            )
-    elif name.startswith('wade'):
-        dataset_cfg = WadeCfg()
-       
-        for s in splits:
-            if s not in dataset_cfg.splits:
-                raise RuntimeError(f'{s} split not found in config')
-            split_cfg = dataset_cfg.splits[s]
-            ann_file = root / split_cfg['ann_filename']
-            parser_cfg = CocoParserCfg(
-                ann_filename=ann_file,
-                has_labels=split_cfg['has_labels']
-            )
-            datasets[s] = dataset_cls(
-                data_dir=root / Path(split_cfg['img_dir']),
-                parser=create_parser(dataset_cfg.parser, cfg=parser_cfg),
-            )
-    elif name.startswith('icra'):
-        dataset_cfg = ICRACfg()
-       
+    elif name in waste_datasets_list:
+        if name.startswith('taco'):
+            dataset_cfg = TACOCfg()
+        elif name.startswith('detectwaste'):
+            dataset_cfg = DetectwasteCfg()
+        elif name.startswith('binary'):
+            dataset_cfg = BinaryCfg()
+        elif name.startswith('multi'):
+            dataset_cfg = BinaryMultiCfg(root=root, ann=ann)
+            dataset_cfg.add_split()
+        elif name.startswith('uav'):
+            dataset_cfg = UAVVasteCfg()
+        elif name.startswith('trashcan'):
+            dataset_cfg = TrashCanCfg()
+        elif name.startswith('drinkwaste'):
+            dataset_cfg = DrinkWasteCfg()
+        elif name.startswith('mju'):
+            dataset_cfg = MJU_WasteCfg()
+        elif name.startswith('wade'):
+            dataset_cfg = WadeCfg()
+        elif name.startswith('icra'):
+            dataset_cfg = ICRACfg()
+        else:
+            assert False, f'Unknown dataset parser ({name})'
         for s in splits:
             if s not in dataset_cfg.splits:
                 raise RuntimeError(f'{s} split not found in config')
