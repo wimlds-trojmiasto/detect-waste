7a40845f1ea0179084607d914ff10ce4982a2284
majsylw <majsylw@onet.eu>
2021-02-20 20:51:15 +0100

    tidy up code

--- MaskRCNN/README.md
+++ MaskRCNN/README.md
@@ -1,4 +1,5 @@
 # PyTorch-MaskRCNN
+![](notebooks/maskrcnn_visualize.png)
 
 A PyTorch implementation of simple Mask R-CNN.
 
@@ -11,23 +12,46 @@ The code is based largely on [TorchVision](https://github.com/pytorch/vision).
 ## Requirements
 
 - **Windows** or **Linux**, with **Python ≥ 3.7**
-
 - **[PyTorch](https://pytorch.org/) ≥ 1.5.0**
-
 - **matplotlib** - visualizing images and results
-
 - **[pycocotools](https://github.com/cocodataset/cocoapi)** - for COCO dataset and evaluation; Windows version is [here](https://github.com/philferriere/cocoapi)
+
+### Neptune
+To track logs (for example training loss) we used [neptune.ai](https://neptune.ai/). If you are interested in logging your experiments there, you should create account on the platform and create new project. Then:
+* Find and set Neptune API token on your system as environment variable (your NEPTUNE_API_TOKEN should be added to ~./bashrc)
+* Add your project_qualified_name name in the `train.py`
+    ```python
+      neptune.init(project_qualified_name = 'YOUR_PROJECT_NAME/detect-waste')
+    ```
+    Currently it is set to private detect-waste neptune space.
+* install neptun-client library
+    ```bash
+      pip install neptune-client
+    ```
+To run experiments with neptune simply add `--neptune` flag during launch `train.py`.
+
+For more check [LINK](https://neptune.ai/how-it-works).
 
 ## Datasets
 
 This repository supports detect-waste datasets (usualy COCO based annotations) with one class - waste.
+
+We expect the directory structure to be the following:
+```
+path/to/repository/
+  annotations/         # annotation json files
+path/to/images/        # all images
+```
+You can modify `datasets/data.py` script to add new dataset and another format of paths for coco annotations type.
+
+Check `detect-waste/annotations/README.md` to verify provided annotations by [Detect Waste in Pomerania team](https://detectwaste.ml/).
 
 ## Training
 
 Simply run:
 
 ```
-python train.py --num_epochs 20 --gpu_id 2 --output_dir /dih4/dih4_2/wimlds/<user_name>
+python3 train.py --num_epochs 26 --gpu_id 2 --output_dir /path/to/save/checkpoints --images_dir /path/to/images
 ```
 or modify the parameters in ```run.sh```, and run:
 
@@ -39,7 +63,13 @@ Note: This is a simple model and only supports one gpu (not distribiuted trainin
 
 ## Evaluation
 
-- Modify the parameters in ```eval.ipynb``` to test the model: TBA
+Simply run on cuda:2:
+
+```python
+python train.py --gpu_id 2 --images_dir /path/to/images --resume /path/to/checkpoint.pth --test-only
+```
+
+or modify the parameters in ```notebooks/eval.ipynb``` to visualize predictions of the model.
 
 ## Performance
 
@@ -49,4 +79,6 @@ Test on Multi detect-waste Segmentation val, on 1 RTX 2080Ti GPU:
 |     model   | backbone     |  dataset | bbox AP@0.5 | bbox AP@0.5:0.95 | mask AP@0.5 | mask AP@0.5:0.95 |
 | :---------: | :---------:  | :------: | :---------: |  :-------------: | :---------: | :--------------: |
 | Mask R-CNN  | ResNet 50    |  Multi   |    27.95    |       16.49      |   23.05     |    12.94         |
-| Mask R-CNN  | ResNetXt 101 |  Multi   |    19.70    |       6.20       |   24.70     |    13.20         |
+| Mask R-CNN  | ResNetXt 101`*` |  Multi   |    19.70    |       6.20       |   24.70     |    13.20         |
+
+* `*` results achived using [mmdetection framework](https://github.com/open-mmlab/mmdetection).
--- MaskRCNN/train.py
+++ MaskRCNN/train.py
@@ -8,10 +8,11 @@ import time
 import os
 import torchvision
 import torch
+from torchvision.models.detection import MaskRCNN
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
 
-import neptune
+from efficientnet_pytorch import EfficientNet
 
 
 def get_args_parser():
@@ -28,22 +29,80 @@ def get_args_parser():
     parser.add_argument(
         '--anno_name',
         help='path to annotation json (part name)',
-        default='/dih4/dih4_home/smajchrowska/detect-waste/'
-                'annotations/annotations_binary_mask_all',
+        default='../annotations/annotations_binary_mask0_all',
         type=str)
-    parser.add_argument('--batch_size', default=2, type=int)
+    parser.add_argument('--resume', default='', help='resume from checkpoint')
+    parser.add_argument(
+        "--test-only",
+        dest="test_only",
+        help="Only test the model",
+        action="store_true",
+    )
+    # Devices
+    parser.add_argument('--batch_size', default=1, type=int)
     parser.add_argument('--num_workers', default=4, type=int)
-    parser.add_argument('--num_epochs', default=10, type=int)
-    parser.add_argument('--gpu_id', default=2, type=int)
+    parser.add_argument('--gpu_id', default=0, type=int)
+    # Learning
+    parser.add_argument('--num_epochs', default=26, type=int)
+    parser.add_argument('--lr', default=0.005, type=float)
+    parser.add_argument('--weight_decay', default=0.0005, type=float)
+    parser.add_argument(
+        '--lr-step-size', default=0, type=int,
+        help='decrease lr every step-size epochs')
+    parser.add_argument(
+        '--lr-steps', default=[16, 22], nargs='+', type=int,
+        help='decrease lr every step-size epochs')
+    parser.add_argument('--lr-gamma', default=0.1, type=float,
+                        help='decrease lr by a factor of lr-gamma')
+    parser.add_argument(
+        '--optimizer',
+        help='Chose type of optimization algorithm, SGD as default',
+        default='SGD', choices=['AdamW', 'SGD'], type=str)
+    # Model
     parser.add_argument('--num_classes', default=2, type=int)
+    parser.add_argument('--model', default='maskrcnn_resnet50_fpn', type=str)
+    # Launch neptune
     parser.add_argument('--neptune', action='store_true')
 
     return parser
 
 
-def get_instance_segmentation_model(num_classes):
-    # load an instance segmentation model pre-trained on COCO
-    model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
+# from https://github.com/lukemelas/EfficientNet-PyTorch
+def round_filters(filters, global_params):
+    """Round number of filters based on depth multiplier."""
+    multiplier = global_params.width_coefficient
+    divisor = global_params.depth_divisor
+    min_depth = global_params.min_depth
+    if not multiplier:
+        return filters
+
+    filters *= multiplier
+    min_depth = min_depth or divisor
+    new_filters = max(
+        min_depth,
+        int(filters + divisor / 2) // divisor * divisor)
+    # Make sure that round down does not go down by more than 10%.
+    if new_filters < 0.9 * filters:
+        new_filters += divisor
+    return int(new_filters)
+
+
+def get_instance_segmentation_model(
+        num_classes, model_name='maskrcnn_resnet50_fpn'):
+    # load a pre-trained model for classification
+    # and return only the features
+    if model_name.startswith('efficientnet'):
+        backbone = EfficientNet.from_pretrained(
+                model_name, num_classes=num_classes,
+                include_top=False)
+        # number of output channels
+        backbone.out_channels = int(round_filters(1280,
+                                                  backbone._global_params))
+        model = MaskRCNN(backbone, num_classes)
+    else:
+        # load an instance segmentation model pre-trained on COCO
+        model = torchvision.models.detection.__dict__[model_name](
+            pretrained=True)
 
     # get the number of input features for the classifier
     in_features = model.roi_heads.box_predictor.cls_score.in_features
@@ -63,11 +122,12 @@ def get_instance_segmentation_model(num_classes):
 if __name__ == '__main__':
     parser = get_args_parser()
     args = parser.parse_args()
-
-    if args.neptune:
+    start_epoch = 0
+    if args.neptune and (not args.resume):
+        import neptune
         # your NEPTUNE_API_TOKEN should be add to ~./bashrc to run this file
         neptune.init(project_qualified_name='detectwaste/maskrcnn')
-        neptune.create_experiment(name='maskrcnn_resnet50_fpn')
+        neptune.create_experiment(name=f'{args.model}')
     else:
         neptune = None
 
@@ -84,8 +144,8 @@ if __name__ == '__main__':
         collate_fn=utils.collate_fn)
 
     data_loader_test = torch.utils.data.DataLoader(
-        dataset_val, batch_size=1, shuffle=False,
-        num_workers=4,
+        dataset_val, batch_size=args.batch_size, shuffle=False,
+        num_workers=args.num_workers,
         collate_fn=utils.collate_fn)
 
     # define model
@@ -97,50 +157,73 @@ if __name__ == '__main__':
     num_classes = args.num_classes
 
     # get the model using our helper function
-    model = get_instance_segmentation_model(num_classes)
+    model = get_instance_segmentation_model(num_classes, args.model)
     # move model to the right device
     model.to(device)
 
     # construct an optimizer
     params = [p for p in model.parameters() if p.requires_grad]
-    optimizer = torch.optim.SGD(params, lr=0.005,
-                                momentum=0.9, weight_decay=0.0005)
+    if args.optimizer == 'AdamW':
+        optimizer = torch.optim.AdamW(params, lr=args.lr,
+                                      weight_decay=args.weight_decay)
+    if args.optimizer == 'SGD':
+        optimizer = torch.optim.SGD(params, lr=args.lr,
+                                    momentum=0.9,
+                                    weight_decay=args.weight_decay)
 
-    # and a learning rate scheduler which decreases the learning rate by
-    # 10x every 3 epochs
-    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
-                                                   step_size=3,
-                                                   gamma=0.1)
+    # and a learning rate scheduler
+    if args.lr_step_size != 0:
+        lr_scheduler = torch.optim.lr_scheduler.StepLR(
+            optimizer, step_size=args.lr_step_size, gamma=args.lr_gamma)
+    else:
+        lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(
+            optimizer, milestones=args.lr_steps, gamma=args.lr_gamma)
 
-    print("Start training")
-    start_time = time.time()
-    for epoch in range(args.num_epochs):
-        # train for one epoch, printing every 10 iterations
-        train_one_epoch(model, optimizer, data_loader,
-                        device, epoch, print_freq=10,
-                        neptune=neptune)
-        # update the learning rate
-        lr_scheduler.step()
-        torch.save({
-                    'epoch': epoch,
-                    'model_state_dict': model.state_dict(),
-                    'optimizer_state_dict': optimizer.state_dict()
-                   }, os.path.join(output_dir, f'checkpoint{epoch:04}.pth'))
+    if args.resume:
+        checkpoint = torch.load(args.resume, map_location=device)
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        start_epoch = checkpoint['epoch'] + 1
+
+    if args.test_only:
         # evaluate on the test dataset
-        coco_evaluator = evaluate(model, data_loader_test, device=device)
-        if neptune is not None:
-            neptune.log_metric(
-                'valid/bbox-mAP@0.5:0.95',
-                coco_evaluator.coco_eval['bbox'].stats[0])
-            neptune.log_metric(
-                'valid/bbox-mAP@0.5',
-                coco_evaluator.coco_eval['bbox'].stats[1])
-            neptune.log_metric(
-                'valid/segm-mAP@0.5:0.95',
-                coco_evaluator.coco_eval['segm'].stats[0])
-            neptune.log_metric(
-                'valid/segm-mAP@0.5',
-                coco_evaluator.coco_eval['segm'].stats[1])
-    total_time = time.time() - start_time
-    total_time_str = str(datetime.timedelta(seconds=int(total_time)))
-    print('Training time {}'.format(total_time_str))
+        print("Start evaluating")
+        dataset_val = build('test', args.images_dir, args.anno_name)
+        data_loader_test = torch.utils.data.DataLoader(
+            dataset_val, batch_size=args.batch_size, shuffle=False,
+            num_workers=args.num_workers,
+            collate_fn=utils.collate_fn)
+        evaluate(model, data_loader_test, device=device)
+    else:
+        print("Start training")
+        start_time = time.time()
+        for epoch in range(start_epoch, args.num_epochs):
+            # train for one epoch, printing every 10 iterations
+            train_one_epoch(model, optimizer, data_loader,
+                            device, epoch, print_freq=10,
+                            neptune=neptune)
+            # update the learning rate
+            lr_scheduler.step()
+            torch.save({
+                        'epoch': epoch,
+                        'model_state_dict': model.state_dict(),
+                        'optimizer_state_dict': optimizer.state_dict()
+                    }, os.path.join(output_dir, f'checkpoint{epoch:04}.pth'))
+            # evaluate on the test dataset
+            coco_evaluator = evaluate(model, data_loader_test, device=device)
+            if neptune is not None:
+                neptune.log_metric(
+                    'valid/bbox-mAP@0.5:0.95',
+                    coco_evaluator.coco_eval['bbox'].stats[0])
+                neptune.log_metric(
+                    'valid/bbox-mAP@0.5',
+                    coco_evaluator.coco_eval['bbox'].stats[1])
+                neptune.log_metric(
+                    'valid/segm-mAP@0.5:0.95',
+                    coco_evaluator.coco_eval['segm'].stats[0])
+                neptune.log_metric(
+                    'valid/segm-mAP@0.5',
+                    coco_evaluator.coco_eval['segm'].stats[1])
+        total_time = time.time() - start_time
+        total_time_str = str(datetime.timedelta(seconds=int(total_time)))
+        print('Training time {}'.format(total_time_str))
