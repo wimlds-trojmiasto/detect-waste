e3f29dd985aba088f538d90ff8f47186d48cf6ec
Chitoyo <midnight.inn@gmail.com>
2021-03-05 12:19:00 +0100

    remove efficient_lightning from the main branch

--- efficientdet_lighning/efficientdet/config/config_utils.py
+++
@@ -1,9 +0,0 @@
-from omegaconf import OmegaConf
-
-
-def set_config_readonly(conf):
-    OmegaConf.set_readonly(conf, True)
-
-
-def set_config_writeable(conf):
-    OmegaConf.set_readonly(conf, False)
--- efficientdet_lighning/efficientdet/data/parsers/__init__.py
+++
@@ -1,2 +0,0 @@
-from .parser_config import OpenImagesParserCfg, CocoParserCfg, VocParserCfg
-from .parser_factory import create_parser
